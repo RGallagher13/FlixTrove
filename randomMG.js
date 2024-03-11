68e35dc7bad6b1d1f3dfd1b4f81e1d868d76efaa
RGallagher13 <rebekahgallagher@hotmail.co.uk>
2024-03-11 19:22:24 +0000

    Update randomMG.js

--- randomMG.js
+++ randomMG.js
@@ -1,12 +1,13 @@
-const apiKey = 'ac7b3e63041ab5c8a88c66a482805cd8';
+const apiKey = 'ac7b3e63041ab5c8a88c66a482805cd8'; //Code which gives me access to the API
 const movieContainer = document.getElementById('movie-container');
 const generateMovieBtn = document.getElementById('generate-movie-btn');
 
-generateMovieBtn.addEventListener('click', fetchRandomMovie);
+generateMovieBtn.addEventListener('click', fetchRandomMovie); //After button is clicked, next part of script runs
 
+//Fetches data from API
 async function fetchRandomMovie() {
-    const randomPage = Math.floor(Math.random() * 500) + 1;
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.desc&page=${randomPage}&certification_country=US&certification.lte=R&with_original_language=en`;
+    const randomPage = Math.floor(Math.random() * 500) + 1; //Picking a random number whcih will be associated with a movie
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&sort_by=popularity.desc&page=${randomPage}&certification_country=US&certification.lte=R&with_original_language=en`; //Where to fetch data from
 
     try {
         const response = await fetch(url);
@@ -21,6 +22,7 @@ async function fetchRandomMovie() {
     }
 }
 
+//Fetches movie details for the random movie selected above
 async function fetchMovieDetails(movieId) {
     const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&append_to_response=release_dates`;
 
@@ -33,6 +35,7 @@ async function fetchMovieDetails(movieId) {
     }
 }
 
+//Displays movie poster and release date from movie selected
 function displayMovie(movie) {
     const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
     const usRelease = movie.release_dates.results.find(release => release.iso_3166_1 === 'US');
